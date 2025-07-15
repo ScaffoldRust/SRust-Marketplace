e4914d5cc10acf3c786bd7059f57178ede4ae3b7
blurbeast <leumasre@gmail.com>
2025-07-15 22:50:18 +0100

    cancel and refund fn done

--- contracts/marketplace-contracts/contracts/installment_payment_contract/src/lib.rs
+++ contracts/marketplace-contracts/contracts/installment_payment_contract/src/lib.rs
@@ -219,7 +219,7 @@ impl InstallmentPayment {
         agreement_id: u128,
     ) -> Result<bool, ContractError> {
         let agreement: (u128, Symbol) = (agreement_id, AGREEMENT);
-        let mut installment_agreement_optional: Option<InstallmentAgreement> =
+        let installment_agreement_optional: Option<InstallmentAgreement> =
             env.storage().persistent().get(&agreement);
         if installment_agreement_optional.is_none() {
             return Err(ContractError::InvalidAgreementId);
@@ -244,11 +244,56 @@ impl InstallmentPayment {
         Ok(true)
     }
 
+    // only seller can cancel the agreement
     pub fn cancel_and_refund_agreement(
         env: Env,
         address: Address,
         agreement_id: u128,
     ) -> Result<bool, ContractError> {
+        let agreement_key: (u128, Symbol) = (agreement_id, AGREEMENT);
+
+        let installment_agreement_optional: Option<InstallmentAgreement> =
+            env.storage().persistent().get(&agreement_key);
+        if installment_agreement_optional.is_none() == true {
+            return Err(ContractError::AgreementNotFOund);
+        }
+
+        let mut installment_agreement: InstallmentAgreement =
+            installment_agreement_optional.unwrap();
+
+        assert!(installment_agreement.is_accepted, "");
+        assert!(!installment_agreement.is_canceled, "");
+        assert!(!installment_agreement.is_finalized, "");
+
+        assert_eq!(
+            installment_agreement.seller, address,
+            "only the seller can cancel agreement"
+        );
+
+        // change the state to true
+        installment_agreement.is_canceled = true;
+
+        // check the total amount paid by the buyer and refund
+        let total_installment_amount_paid: u128 = installment_agreement.amount_paid;
+
+        if total_installment_amount_paid > 0 {
+            // check if the amount paid is greater than 0
+            let token_contract: TokenClient =
+                token::TokenClient::new(&env, &installment_agreement.token);
+
+            // call on the token client
+
+            token_contract.transfer(
+                &env.current_contract_address(),
+                &installment_agreement.buyer,
+                &(total_installment_amount_paid as i128),
+            );
+        }
+
+        //save to the storage
+        env.storage()
+            .persistent()
+            .set(&agreement_key, &installment_agreement);
         Ok(true)
     }
 
