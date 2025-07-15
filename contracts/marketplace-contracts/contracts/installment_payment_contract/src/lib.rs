31d3ce4cecc6b4413e37f1dad48a605784e0bd39
blurbeast <leumasre@gmail.com>
2025-07-15 22:28:59 +0100

    formatted

--- contracts/marketplace-contracts/contracts/installment_payment_contract/src/lib.rs
+++ contracts/marketplace-contracts/contracts/installment_payment_contract/src/lib.rs
@@ -1,20 +1,21 @@
-
 #![no_std]
 
 mod test;
 
-use soroban_sdk::{contract, contracterror, contractimpl, contracttype, symbol_short, token::{self, TokenClient}, Address, Env, String, Symbol, Vec };
+use soroban_sdk::{
+    contract, contracterror, contractimpl, contracttype, symbol_short,
+    token::{self, TokenClient},
+    Address, Env, String, Symbol, Vec,
+};
 // use soroban_sdk::testutils::arbitrary::std::println;
 
 const ADMIN: Symbol = symbol_short!("i_p_admin"); // length cannot be more than 9, hence, i = installment, p = payment,
 const AGREEMENT_ID: Symbol = symbol_short!("agree_id");
 const AGREEMENT: Symbol = symbol_short!("agreement");
 
-
 #[contracterror]
 #[derive(Debug, Clone, PartialEq)]
 pub enum ContractError {
-
     AlreadyInstantiated = 1,
     InvalidAmount = 2,
     DuplicateUsers = 3,
@@ -23,18 +24,15 @@ pub enum ContractError {
     InvalidAgreementId = 6,
     NotAuthorized = 7,
     AgreementNotFOund = 8,
-
 }
 #[contract]
 pub struct InstallmentPayment;
-
 
 #[contracttype]
 pub struct PaidHistory {
     pub amount: u128,
     pub timeline: u64,
 }
-
 
 #[contracttype]
 pub struct InstallmentAgreement {
@@ -43,7 +41,7 @@ pub struct InstallmentAgreement {
     pub seller: Address,
 
     pub is_accepted: bool,
-    
+
     pub amount_paid: u128,
     pub paid_history: Vec<PaidHistory>,
     pub total_amount: u128,
@@ -60,21 +58,17 @@ pub struct InstallmentAgreement {
     pub token: Address,
 }
 
-
-
 #[contractimpl]
 impl InstallmentPayment {
-
     // initialize the contract
     // @params: env of type Env is the environment variable from soroban
     // @params: admin which would act as the owner of this contract
-    pub fn initialize(env: Env, admin: Address ) -> Result<Address, ContractError> {
-
+    pub fn initialize(env: Env, admin: Address) -> Result<Address, ContractError> {
         // we only want to create just one admin inn this contract
         let admin_exist: bool = env.storage().persistent().has(&ADMIN);
         // println!("admin exist {:p}, {:#}", &admin_exist, &admin_exist);
         if admin_exist {
-             return Err(ContractError::AlreadyInstantiated);
+            return Err(ContractError::AlreadyInstantiated);
             // panic!("");
         }
         // check auth
@@ -85,7 +79,16 @@ impl InstallmentPayment {
 
     // since it is the buyer that want to pay on installment , it best we allow buyer to create the agreement then choose list pf Arbitrator provided by the platform
     // if the seller is satisfied with the agreement , the seller accepts/ agree to the agreement which then buyer can start making deposits
-    pub fn create_installment_agreement(env: Env, seller: Address, buyer: Address, amount: u128, deadline: u64, arbitrator: Address, token: Address, description: String) -> Result<bool, ContractError> {
+    pub fn create_installment_agreement(
+        env: Env,
+        seller: Address,
+        buyer: Address,
+        amount: u128,
+        deadline: u64,
+        arbitrator: Address,
+        token: Address,
+        description: String,
+    ) -> Result<bool, ContractError> {
         buyer.require_auth();
 
         // confirm the deadline and amount
@@ -95,7 +98,7 @@ impl InstallmentPayment {
 
         // ensure buyer is not the seller
         if &buyer == &seller {
-            return  Err(ContractError::DuplicateUsers);
+            return Err(ContractError::DuplicateUsers);
         }
 
         if &buyer == &arbitrator || &seller == &arbitrator {
@@ -124,31 +127,51 @@ impl InstallmentPayment {
             is_canceled: false,
             arbitrator,
             description,
-            token
+            token,
         };
 
         //save the agreement
-        let agreement_key: (u128, Symbol) =  (new_agreement_id, AGREEMENT);
-        env.storage().persistent().set(&AGREEMENT_ID, &new_agreement_id);
+        let agreement_key: (u128, Symbol) = (new_agreement_id, AGREEMENT);
+        env.storage()
+            .persistent()
+            .set(&AGREEMENT_ID, &new_agreement_id);
 
-        env.storage().persistent().set(&agreement_key, &install_agreement);
+        env.storage()
+            .persistent()
+            .set(&agreement_key, &install_agreement);
 
         Ok(true)
     }
-    
-    pub fn pay_on_installment(env: Env, buyer_address: Address, installment_amount: u128, agreement_id: u128) -> Result<bool, ContractError> {
-        let agreement_key: (u128 ,Symbol, ) = (agreement_id, AGREEMENT);
 
-        let installment_agreement_optional: Option<InstallmentAgreement> = env.storage().persistent().get(&agreement_key);
+    pub fn pay_on_installment(
+        env: Env,
+        buyer_address: Address,
+        installment_amount: u128,
+        agreement_id: u128,
+    ) -> Result<bool, ContractError> {
+        let agreement_key: (u128, Symbol) = (agreement_id, AGREEMENT);
+
+        let installment_agreement_optional: Option<InstallmentAgreement> =
+            env.storage().persistent().get(&agreement_key);
         if &installment_agreement_optional.is_none() == &true {
             return Err(ContractError::AgreementNotFOund);
         }
 
-        let mut installment_agreement: InstallmentAgreement = installment_agreement_optional.unwrap();
+        let mut installment_agreement: InstallmentAgreement =
+            installment_agreement_optional.unwrap();
 
-        assert!(&installment_agreement.is_accepted, "agreement not accepted yet");
-        assert!(!&installment_agreement.is_canceled, "agreement has been cancelled");
-        assert!(!&installment_agreement.is_finalized, "agreement has been finalized");
+        assert!(
+            &installment_agreement.is_accepted,
+            "agreement not accepted yet"
+        );
+        assert!(
+            !&installment_agreement.is_canceled,
+            "agreement has been cancelled"
+        );
+        assert!(
+            !&installment_agreement.is_finalized,
+            "agreement has been finalized"
+        );
         assert!(&installment_agreement.deadline > &env.ledger().timestamp());
 
         assert!(installment_amount > 0, "amount cannot be zero");
@@ -159,57 +182,79 @@ impl InstallmentPayment {
         let token_contract: TokenClient = token::TokenClient::new(&env, &token_address);
         let user_balance: i128 = token_contract.balance(&buyer_address);
 
-        assert!(user_balance >= installment_amount as i128, "insufficient balance");
+        assert!(
+            user_balance >= installment_amount as i128,
+            "insufficient balance"
+        );
 
-        token_contract.transfer(&buyer_address, &env.current_contract_address(), &(installment_amount as i128));
+        token_contract.transfer(
+            &buyer_address,
+            &env.current_contract_address(),
+            &(installment_amount as i128),
+        );
 
         // create the payment history
-        let payment_history: PaidHistory = PaidHistory { amount: installment_amount, timeline: env.ledger().timestamp() };
-        // update the agreement 
-        
-        installment_agreement.paid_history.push_back(payment_history);
+        let payment_history: PaidHistory = PaidHistory {
+            amount: installment_amount,
+            timeline: env.ledger().timestamp(),
+        };
+        // update the agreement
+
+        installment_agreement
+            .paid_history
+            .push_back(payment_history);
         installment_agreement.amount_paid += installment_amount;
 
         Ok(true)
     }
 
-    pub fn finalize_agreement(env: Env, agreement_id: u128, ) -> Result<bool, ContractError> {
-
+    pub fn finalize_agreement(env: Env, agreement_id: u128) -> Result<bool, ContractError> {
         Ok(true)
     }
 
-    pub fn accept_installment_agreement(env: Env, seller: Address, accept_agreement: bool, agreement_id: u128 ) -> Result<bool, ContractError> {
-        let agreement: (u128 ,Symbol, ) = (agreement_id, AGREEMENT);
-        let mut installment_agreement_optional: Option<InstallmentAgreement> = env.storage().persistent().get(&agreement);
+    pub fn accept_installment_agreement(
+        env: Env,
+        seller: Address,
+        accept_agreement: bool,
+        agreement_id: u128,
+    ) -> Result<bool, ContractError> {
+        let agreement: (u128, Symbol) = (agreement_id, AGREEMENT);
+        let mut installment_agreement_optional: Option<InstallmentAgreement> =
+            env.storage().persistent().get(&agreement);
         if installment_agreement_optional.is_none() {
             return Err(ContractError::InvalidAgreementId);
         }
-        let mut installment_agreement: InstallmentAgreement = installment_agreement_optional.unwrap();
-        
-        if &seller  != &installment_agreement.seller { 
+        let mut installment_agreement: InstallmentAgreement =
+            installment_agreement_optional.unwrap();
+
+        if &seller != &installment_agreement.seller {
             return Err(ContractError::NotAuthorized);
         }
 
         assert!(!&installment_agreement.is_accepted);
         assert!(!&installment_agreement.is_canceled);
         assert!(!&installment_agreement.is_finalized);
-        
+
         installment_agreement.is_accepted = accept_agreement;
-        
-        
-        env.storage().persistent().set(&agreement, &installment_agreement);
+
+        env.storage()
+            .persistent()
+            .set(&agreement, &installment_agreement);
 
         Ok(true)
     }
 
-    pub fn cancel_and_refund_agreement(env: Env, address: Address, agreement_id: u128) -> Result<bool, ContractError> {
+    pub fn cancel_and_refund_agreement(
+        env: Env,
+        address: Address,
+        agreement_id: u128,
+    ) -> Result<bool, ContractError> {
         Ok(true)
     }
 
     pub fn get_installment_agreement(env: Env, agreement_id: u128) -> Option<InstallmentAgreement> {
-
         // Err(String::from_str(&env, ""))
-        let agreement_key: (u128 ,Symbol, ) = (agreement_id, AGREEMENT);
+        let agreement_key: (u128, Symbol) = (agreement_id, AGREEMENT);
         env.storage().persistent().get(&agreement_key)
     }
 }
--- contracts/marketplace-contracts/contracts/installment_payment_contract/src/test.rs
+++ contracts/marketplace-contracts/contracts/installment_payment_contract/src/test.rs
@@ -1,19 +1,10 @@
-
-
-
-
 #[cfg(test)]
 mod test {
 
-    use soroban_sdk::{
-        testutils::Address as _,
-        Address, Env
-    };
-    use crate::{
-        InstallmentPayment, InstallmentPaymentClient, ContractError
-    };
+    use crate::{ContractError, InstallmentPayment, InstallmentPaymentClient};
+    use soroban_sdk::{testutils::Address as _, Address, Env};
 
-    fn create_contract_variables() -> (Env, Address, Address ) {
+    fn create_contract_variables() -> (Env, Address, Address) {
         let env: Env = Env::default();
         env.mock_all_auths();
 
@@ -26,11 +17,11 @@ mod test {
     fn test_initialized_contract() {
         // register the contract
 
-        let(env, contract_address, admin) = create_contract_variables();
+        let (env, contract_address, admin) = create_contract_variables();
         // env.register_contract(None, InstallmentPayment); the register contract method of the env has been deprecated
 
         let installment_payment_instance = InstallmentPaymentClient::new(&env, &contract_address); // the client with the new function return the instance of the contract
-        // installment_payment_instance
+                                                                                                   // installment_payment_instance
         let initialized_result = installment_payment_instance.initialize(&admin);
 
         assert_eq!(initialized_result, admin)
@@ -39,12 +30,12 @@ mod test {
     #[test]
     #[should_panic]
     fn test_cannot_initialize_more_than_once() {
-        let(env, contract_address, admin) = create_contract_variables();
+        let (env, contract_address, admin) = create_contract_variables();
         // env.register_contract(None, InstallmentPayment); the register contract method of the env has been deprecated
 
         let installment_payment_instance = InstallmentPaymentClient::new(&env, &contract_address); // the client with the new function return the instance of the contract
-        // installment_payment_instance
-        // let contract_instance = installment_payment_instance.initialize(&admin);
+                                                                                                   // installment_payment_instance
+                                                                                                   // let contract_instance = installment_payment_instance.initialize(&admin);
 
         installment_payment_instance.initialize(&admin);
         installment_payment_instance.initialize(&admin);
